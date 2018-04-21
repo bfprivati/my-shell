591b79467c6bd45631e115767df655479252235b
Stefany Lacroux <stefanylacroux@hotmail.com>
2018-04-21 16:04:12 -0300

    dup2.c

--- Projeto/dup2.c
+++ Projeto/dup2.c
@@ -1,18 +1,32 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+/*
+void func1()
+{
 
+}
+*/
 int main()
 {
-  char * filename = "cores.txt";
-  int fd = open(filename, O_RDONLY, 0);
-  dup2(fd, 0);
-  close(fd);
-  char * params[] = {"more", NULL};
-  execvp("more",params);
+  char * in = "cores.txt";
+  char * out = "saida.txt";
+
+  int fdin = open(in, O_RDONLY, 0);
+  int fdout = open(out, O_WRONLY, 0);
+
+  dup2(fdin, 0);
+  dup2(fdout, 1);
+
+  close(fdin);
+  close(fdout);
+  
+  char * params[] = {"ls", NULL};
+  execvp("ls",params);
 
   return 0;
 }
