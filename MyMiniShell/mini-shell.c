57d8abdc151fdfab66803979e75cd682260f1a90
Bianca Privati <bfprivati@gmail.com>
2018-05-01 18:01:07 -0300

    funcoes

--- MyMiniShell/mini-shell.c
+++ MyMiniShell/mini-shell.c
@@ -7,30 +7,8 @@
 
 // >
 void func_out(){
-  char * params[] = {"ls", NULL};
-  execvp("ls",params);
-}
-
-// >>
-void func_append(){
-  char * params[] = {"ls", NULL};
-  execvp("ls",params);
-}
-
-// 2> 
-void func_append_error(){
-
-}
-
-// 2>>
-void func_append_error2(){
-
-}
-
-int main()
-{
   char * in = "in.txt";
-  char * out = "out.txt";
+  char * out = "saida.txt";
 
   int fdin = open(in, O_RDONLY, 0);
   int fdout = open(out, O_WRONLY, 0);
@@ -41,8 +19,46 @@ int main()
   close(fdin);
   close(fdout);
  
-  //char * params[] = {"ls", ">>", EOF};
-  //exec("ls",params);
+  char * params[] = {"sort",NULL};
+  execvp("sort",params);
+}
+
+// >>
+void func_append(){
+  char * in = "in.txt";
+  char * out = "saida.txt";
+
+  int fdin = open(in, O_RDONLY, 0);
+  int fdout = open(out, O_WRONLY|O_APPEND, 0);
+
+  dup2(fdin, 0);
+  dup2(fdout, 1);
+
+  close(fdin);
+  close(fdout);
+ 
+  char * params[] = {"sort",NULL};
+  execvp("sort",params);
+}
+
+/*
+// 2> 
+void func_error(){
+
+}
+
+// 2>>
+void func_error_append(){
+
+} 
+*/
+
+int main()
+{
+  func_out();
+  func_append();
+  func_error();
+  func_error_append();
 
   return 0;
 }
