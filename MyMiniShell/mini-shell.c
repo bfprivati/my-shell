35cc9f54c2fb2d5dc5d29e11e8c2c5d6a0bbab4d
Bianca Privati <bfprivati@gmail.com>
2018-05-01 18:29:48 -0300

    Update mini-shell.c

--- MyMiniShell/mini-shell.c
+++ MyMiniShell/mini-shell.c
@@ -41,17 +41,50 @@ void func_append(){
   execvp("sort",params);
 }
 
-/*
 // 2> 
 void func_error(){
+  char * in = "in.txt";
+  char * out = "out.txt";
+  char * error = "error.txt";
 
+  int fdin = open(in, O_RDONLY, 0);
+  int fdout = open(out, O_WRONLY|O_APPEND, 0);
+  int fderror = open(error, O_WRONLY, 0);
+
+  dup2(fdin, 0);
+  dup2(fdout, 1);
+  dup2(fderror, 2);
+
+  close(fdin);
+  close(fdout);
+  close(fderror);
+
+  char * params[] = {"rm", "saidaaaaa.txt", NULL};
+  execvp("rm",params);
 }
 
 // 2>>
 void func_error_append(){
+void func_error(){
+  char * in = "in.txt";
+  char * out = "out.txt";
+  char * error = "error.txt";
 
+  int fdin = open(in, O_RDONLY, 0);
+  int fdout = open(out, O_WRONLY|O_APPEND, 0);
+  int fderror = open(error, O_WRONLY|O_APPEND, 0);
+
+  dup2(fdin, 0);
+  dup2(fdout, 1);
+  dup2(fderror, 2);
+
+  close(fdin);
+  close(fdout);
+  close(fderror);
+
+  char * params[] = {"rm", "saidaaaaa.txt", NULL};
+  execvp("rm",params);
 } 
-*/
 
 int main()
 {
