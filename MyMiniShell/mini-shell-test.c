a160106180a756f4baaaa65ea866db154cf76e0a
Bianca Privati <bfprivati@gmail.com>
2018-05-01 17:48:20 -0300

    sort

--- MyMiniShell/mini-shell-test.c
+++ MyMiniShell/mini-shell-test.c
@@ -30,20 +30,20 @@ void func_append_error2(){
 
 int main()
 {
-  //char * in = "in.txt";
+  char * in = "in.txt";
   char * out = "saida.txt";
 
-  //int fdin = open(in, O_RDONLY, 0);
+  int fdin = open(in, O_RDONLY, 0);
   int fdout = open(out, O_APPEND, 0);
 
-  //dup2(fdin, 0);
+  dup2(fdin, 0);
   dup2(fdout, 1);
 
-  //close(fdin);
+  close(fdin);
   close(fdout);
  
-  char * params[] = {"ls",NULL};
-  execvp("ls",params);
+  char * params[] = {"sort",NULL};
+  execvp("sort",params);
 
   return 0;
 }
