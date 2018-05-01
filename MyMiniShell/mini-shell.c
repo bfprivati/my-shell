8fdc8440246f646bb1500c53c742b881fa1b25ad
Bianca Privati <bfprivati@gmail.com>
2018-05-01 17:35:25 -0300

    minishell

---
+++ MyMiniShell/mini-shell.c
@@ -0,0 +1,48 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+// >
+void func_out(){
+  char * params[] = {"ls", NULL};
+  execvp("ls",params);
+}
+
+// >>
+void func_append(){
+  char * params[] = {"ls", NULL};
+  execvp("ls",params);
+}
+
+// 2> 
+void func_append_error(){
+
+}
+
+// 2>>
+void func_append_error2(){
+
+}
+
+int main()
+{
+  char * in = "in.txt";
+  char * out = "out.txt";
+
+  int fdin = open(in, O_RDONLY, 0);
+  int fdout = open(out, O_WRONLY, 0);
+
+  dup2(fdin, 0);
+  dup2(fdout, 1);
+
+  close(fdin);
+  close(fdout);
+ 
+  //char * params[] = {"ls", ">>", EOF};
+  //exec("ls",params);
+
+  return 0;
+}
